fb66491f4e060b45950226136df64c17194ef029
r-i-c-h <rich@richwerden.com>
2023-06-01 19:31:07 -0400

    🔨WIP? 🐛:Github Login on Vercel

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -14,7 +14,6 @@ import { env } from "~/env.mjs";
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
  * object and keep type safety.
- *
  * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
  */
 declare module "next-auth" {
@@ -27,17 +26,31 @@ declare module "next-auth" {
   }
 
   // interface User {
-  //   // ...other properties
-  //   // role: UserRole;
+  // ...other properties
+  // role: UserRole;
   // }
 }
 
 /**
  * Options for NextAuth.js used to configure adapters, providers, callbacks, etc.
- *
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: String(env.GITHUB_ID),
+      clientSecret: String(env.GITHUB_SECRET),
+    }),
+    /**...add more providers here.
+     * Example:
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
@@ -47,26 +60,6 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    GithubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-    })
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
-  ],
 };
 
 /**
