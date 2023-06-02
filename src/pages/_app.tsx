c0cb446bdbbb23603e6cae33fcc945b9caf8c700
r-i-c-h <rich@richwerden.com>
2023-06-02 17:38:32 -0400

    Moves <HEAD> content to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+import Head from "next/head";
 import "~/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -10,6 +11,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>T3witter Social</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="A clone of a popular social newsfeed app that people post to." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
