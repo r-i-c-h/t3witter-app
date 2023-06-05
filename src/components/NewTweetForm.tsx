3e25aaa50b5300e9254f4e085ed52e9990e6e470
r-i-c-h <rich@richwerden.com>
2023-06-05 15:21:39 -0400

    adds new msg backend routing  del example code

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -2,18 +2,34 @@ import { useSession } from "next-auth/react";
 import Button from "./Button";
 import ProfileImage from "./ProfileImage";
 import ExpandingTextArea from "./ExpandingTextArea";
-import { useState } from "react";
+import { type FormEvent, useState } from "react";
+import { api } from "~/utils/api";
+
 
 export default function NewTweetForm() {
   const session = useSession();
   const [inputValue, setInputValue] = useState('');
-  //!!
-  //TODO:
-  // if (session.status !== "authenticated") return null;
+
+  const createNewPost = api.tweet.create.useMutation({
+    onSuccess: (newTwit) => {
+      console.log(newTwit)
+      setInputValue("Ku");
+    }
+  })
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const content = inputValue;
+    createNewPost.mutate({ content });
+  }
+
+  //TODO: MAKE PRETTIER
+  if (session.status !== "authenticated") return null;
+
 
   return (
     <>
-      <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
         <div className="flex gap-4">
           <ProfileImage imageSrc={session.data?.user.image} />
           <ExpandingTextArea inputValue={inputValue} setInputValue={setInputValue} />
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,13 +1,12 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { tweetRouter } from "~/server/api/routers/tweet";
 
 /**
  * This is the primary router for your server.
- *
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  tweet: tweetRouter,
 });
 
 // export type definition of API
