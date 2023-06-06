b2818011a03ed708b95a60177cb7bb3166555bed
r-i-c-h <rich@richwerden.com>
2023-06-06 17:25:03 -0400

    add UNTESTED(:warn:) GET many() tweets to Prisma

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,11 +1,63 @@
+import { type User, type Like } from "@prisma/client";
 import { z } from "zod";
 import {
   createTRPCRouter,
-  // publicProcedure,
+  publicProcedure,
   protectedProcedure,
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
+  infiniteFeed: publicProcedure.input(
+    z.object({
+      limit: z.number().optional(),
+      cursor: z.object({ id: z.string(), createdAt: z.date() }).optional()
+    }))
+    .query(
+      async ({ input: { limit = 20, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+
+        const data = await ctx.prisma.tweet.findMany({
+          take: limit + 1, // if there *is* a tweet beyond the limit, grab it to set up the nextCursor below
+          cursor: cursor ? { createdAt_id: cursor } : undefined,
+          orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+          select: {
+            id: true,
+            content: true,
+            createdAt: true,
+            _count: { select: { likes: true } },
+            likes: currentUserId == null ? false : { where: { userId: currentUserId } },
+            user: {
+              select: {
+                name: true,
+                id: true,
+                image: true
+              }
+            }
+          }
+        });
+
+        /** Setup Next Cursor for data 'pagination' */
+        let nextCursor: typeof cursor | undefined;
+        if (data.length > limit) {
+          const nextItem = data.pop();
+          if (nextItem != null) { // @ts is unhappy with "!=="? Wants "!="" instead 🤷‍♂️
+            nextCursor = { id: nextItem?.id, createdAt: nextItem?.createdAt }
+          }
+        }
+        /** Change Tweet-Data formatting to make front-end implementation a bit nicer **/
+        const tweets = data.map((tweet) => {
+          const { id, content, createdAt, _count } = tweet;
+          const countVal = _count as { likes: number } // Makes @ts happy
+          const likeCount = countVal.likes;
+          const likesArr = tweet.likes as Like[]; // Makes @ts happy
+          const likedByMe: boolean = likesArr.length > 0;
+          const user = tweet.user as User // Makes @ts happy
+          return { id, content, createdAt, user, likeCount, likedByMe };
+        })
+
+        return { nextCursor, tweets };
+      }
+    ),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
