b2c24717140590e0512bc7eeeae9145fa88e0b44
r-i-c-h <rich@richwerden.com>
2023-06-07 15:38:39 -0400

    triv: 🗑️cruft

--- src/components/TweetCard.tsx
+++ src/components/TweetCard.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import Image from "next/image";
 import { type Tweet } from './InfiniteTweetsList';
 import ProfileImage from './ProfileImage';
 import { useSession } from 'next-auth/react';
