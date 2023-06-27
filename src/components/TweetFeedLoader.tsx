6e2289a96f8de9724611104788a2c21238c4db34
r-i-c-h <rich@richwerden.com>
2023-06-26 23:57:30 -0400

    Update Loading Cards/Spinner, 86s shimmer effect

--- src/components/TweetFeedLoader.tsx
+++ src/components/TweetFeedLoader.tsx
@@ -12,19 +12,19 @@ export default function TweetFeedLoader() {
 
 function LoadingSkeletonTweetCard() {
   return (
-    <div className="flex animate-pulse border-b px-4 py-4">
-      <div className="ml-4">
-        <div data-placeholder className="w-16 h-16 block bg-gray-200 rounded-full dark:bg-gray-700"> </div>
+    <div className="flex border-b px-4 py-4 bg-amber-50">
+      <div className="ml-4 animate-pulse">
+        <div data-placeholder className="w-16 h-16 block bg-amber-800 rounded-full dark:bg-amber-700"> </div>
       </div>
 
-      <div className="ml-4 w-full">
+      <div className="ml-4 w-full animate-pulse">
         <div className="flex items-center">
-          <div data-placeholder className="inline-block h-5 bg-gray-200 rounded-md dark:bg-gray-700 w-2/5"></div>
-          <div data-placeholder className="ml-2 inline-block h-3 bg-gray-200 rounded-md dark:bg-gray-700 w-1/5"></div>
+          <div data-placeholder className="inline-block h-5 bg-amber-800 rounded-md dark:bg-amber-700 w-2/5"></div>
+          <div data-placeholder className="ml-2 inline-block h-3 bg-amber-800 rounded-md dark:bg-amber-700 w-1/5"></div>
         </div>
         <ul className="mt-2 space-y-2">
-          <li data-placeholder className="w-5/6 h-5 bg-gray-200 rounded-md dark:bg-gray-700"> </li>
-          <li data-placeholder className="w-1/6 h-5 bg-gray-200 rounded-md dark:bg-gray-700"> </li>
+          <li data-placeholder className="w-5/6 h-5 bg-amber-800 rounded-md dark:bg-amber-700"> </li>
+          <li data-placeholder className="w-1/6 h-5 bg-amber-800 rounded-md dark:bg-amber-700"> </li>
         </ul>
       </div>
     </div>
