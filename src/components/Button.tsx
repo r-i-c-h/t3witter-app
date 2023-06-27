9cbb279dc9b9290ef4ef739e25cfc8486ecd0c18
r-i-c-h <rich@richwerden.com>
2023-06-26 23:35:59 -0400

    🎨 Better Button Hover Styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,12 +13,12 @@ type ButtonProps = {
 export default function Button({ type = "submit", small = false, gray = false, className = "", ...props }: ButtonProps) {
   const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold"
   const colorClasses = gray
-    ? "bg-gray-400 hover:bg-gray-300 focus-visible:bg-gray-300"
-    : "bg-amber-900 hover:bg-amber-700 focus-visible:bg-amber-700 focus-visible:outline-amber-900 active:bg-amber-200"
+    ? "bg-slate-500 hover:bg-slate-300 focus-visible:bg-slate-300"
+    : "bg-amber-900 hover:bg-amber-600 focus-visible:bg-amber-600 focus-visible:outline-amber-700 focus-visible:outline-4 active:bg-amber-200"
   return (
     <button
       type={type}
-      className={`w-auto text-white active:text-black active:shadow-none rounded-md hover:text-opacity-50 transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
+      className={`w-auto text-white active:text-black active:shadow-none active:outline active:outline-2 rounded-md hover:text-opacity-50 transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
       {...props}
     ></button>
   );
