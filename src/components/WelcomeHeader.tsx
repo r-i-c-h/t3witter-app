af0d78db25184f950a9642ff867f3bbe69ecbb21
r-i-c-h <rich@richwerden.com>
2023-06-29 03:40:14 -0400

    ♻️🛠️ move Welcome Header to own component for further restyling

---
+++ src/components/WelcomeHeader.tsx
@@ -0,0 +1,22 @@
+import { useSession } from "next-auth/react";
+
+export default function WelcomeHeader() {
+  const session = useSession();
+
+  if (session.status === "authenticated") { // AUTH PATH
+    const baseName = session.data?.user.name;
+    const name = baseName?.split(' ')[0];
+    return (
+      <h1 className={`text-2xl lg:text-left mb-2 px-4 py-1 text-center text-amber-900 font-bold`}>
+        Welcome <span className="">{name}</span>
+      </h1>
+    )
+
+  } else { // PUBLIC PATH
+    return (
+      <h1 className={`text-3xl text-center px-4 pb-8 pt-1 text-amber-900 font-bold`}>
+        <span className="uppercase font-sans">Welcome To</span> <span className="uppercase font-mono">T3witter</span>
+      </h1>
+    );
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,46 +5,38 @@ import NewTweetForm from "~/components/NewTweetForm";
 import RecentTweets from "~/components/RecentTweets";
 import FollowingTweets from "~/components/FollowingTweets";
 import LoadingSpinner from "~/components/LoadingSpinner";
+import WelcomeHeader from "~/components/WelcomeHeader";
 
 const TABS = ["Recent", "Following"] as const; // Doesn't work as pure Enum?
 
 const Home: NextPage = () => {
   const session = useSession();
-  const [selectedTab, setSelectedTab] = useState<(typeof TABS)[number]>("Recent");
+  const [selectedTab, setSelectedTab] = useState<(typeof TABS)[number]>("Recent"); // <~~Sets "Recent" as default for Unauth'd Public Feed
 
-  // TS wants a login function to have a void-wrapper as below:
-  //>> const handleLogin = () => { void signIn(); }
+  /**
+  ** NOTE: For a fully-private (non-Twitter) experience
+  ** protect feeds and implement Login+Logout/signIn() here, not only in Sidebar
+  ** Be aware, if you do so, TS wants login function to have a void-wrapper as below:
+  ** >> const handleLogin = () => { void signIn(); }
+  **/
 
-  // For a fully-private non-Twitter experience protect feeds and implement Login+Logout here, not only in Sidebar
-  //  if (session.status === "unauthenticated") {
-  //  ...LOGIN WIDGET ala... return <h2>Please <button onClick={handleLogin}>Log In</button></h2>
-  //  }
-
+  /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+  /**** LOADING STATE ****/
   if (session.status === "loading") { return <LoadingSpinner big={true} /> }
-
-  let welcomeString;
-  if (session.status === "authenticated") {
-    const baseName = session.data?.user.name;
-    welcomeString = baseName?.split(' ')[0];
-  } else {
-    welcomeString = "to T3witter"
-  }
+  /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
   return (
     <>
       <header className="sticky top-0 z-10 bg-gradient-to-b from-amber-200 to-amber-50 pt-2">
-        <h1 className="lg:text-left text-center mb-2 px-4 py-1 text-2xl text-amber-900 font-bold">Welcome <span className="">{welcomeString}</span></h1>
+        <WelcomeHeader />
         {session.status === "authenticated" && (<>
           <div className="flex">
             {TABS.map(tab => {
               return <button key={tab}
                 onClick={() => setSelectedTab(tab)}
                 className={
-                  `flex-grow p-2 border-b-4 hover:bg-amber-400 focus-visible:bg-amber-400 hover:text-black focus-visible:text-black transition-colors duration-300 hover:border-amber-800
-
-                  ${tab === selectedTab ?
-                    `border-amber-500 text-amber-950 font-semibold`
-                    : `border-amber-200 text-amber-900`}
+                  `font-serif text-lg flex-grow p-2 border-b-4 hover:bg-amber-400 focus-visible:bg-amber-400 hover:text-black focus-visible:text-black transition-colors duration-300 hover:border-amber-800
+                  ${tab === selectedTab ? `border-amber-500 text-amber-950 font-semibold` : `border-amber-200 text-amber-900`}
                   `
                 }>
                 {tab}
