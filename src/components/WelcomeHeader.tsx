70959eb53706c965868aac8709b486085ca33c69
r-i-c-h <rich@richwerden.com>
2023-07-02 00:27:57 -0500

    adds:animated SVG to unauth'd Welcome + Header-🎨

--- src/components/WelcomeHeader.tsx
+++ src/components/WelcomeHeader.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react";
+import { WelcomeHeaderSVG } from "./WelcomeHeaderSVG";
 
 export default function WelcomeHeader() {
   const session = useSession();
@@ -7,16 +8,21 @@ export default function WelcomeHeader() {
     const baseName = session.data?.user.name;
     const name = baseName?.split(' ')[0];
     return (
-      <h1 className={`text-2xl lg:text-left mb-2 px-4 py-1 text-center text-amber-900 font-bold`}>
-        Welcome <span className="">{name}</span>
+      <h1 className={`text-2xl lg:text-left mb-2 px-4 py-1 text-center text-amber-900 font-bold motion-safe:animate-apparate`}>
+        Welcome <span className="text-3xl">{name}</span>
       </h1>
     )
 
   } else { // PUBLIC PATH
     return (
-      <h1 className={`text-3xl text-center px-4 pb-8 pt-1 text-amber-900 font-bold`}>
-        <span className="uppercase">Welcome To</span> <span className="uppercase font-mono">T3witter</span>
-      </h1>
+      <div className="">
+        <h1 className={`text-3xl md:w-full md:flex items-center justify-center gap-4 pb-1 text-center shadow-xl text-amber-900 font-bold`}>
+          <div className="uppercase motion-safe:animate-apparate">Welcome To</div>
+          <div className="flex items-center justify-center pb-1 ">
+            <WelcomeHeaderSVG />
+          </div>
+        </h1>
+      </div>
     );
   }
 }
