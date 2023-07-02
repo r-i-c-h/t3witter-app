08b1fcee6e7bff6a729bb90610fa9d8f15936e93
r-i-c-h <rich@richwerden.com>
2023-07-02 00:25:52 -0500

    adds: custom animations to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,25 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        apparate: {
+          '0%': { transform: 'translate(0, 8px)', opacity: '0.2' },
+          '100%': { transform: 'translate(0, 0px)', opacity: '1' },
+        },
+        amber_fill: {
+          '0%': { opacity: '0.7', fill: '#FCD34D' },
+          '100%': { opacity: '1', fill: '#FCD34D' },
+        },
+        complete_line: {
+          '100%': { strokeDashoffset: 0 },
+        },
+      },
+      animation: {
+        'apparate': 'apparate 1s forwards',
+        'draw-welcome': '6000ms cubic-bezier(0.83, 0, 0.17, 1) forwards complete_line, 2000ms ease-out infinite alternate amber_fill'
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
